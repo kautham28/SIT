a9aed21cc3086c9e1ef2f68e9df284b0245ceec1
Vino200003 <vinoshii03@gmail.com>
2025-04-06 15:36:55 +0530

    Login and otp page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Registration from './pages/Registration/Registration';
 import RegistrationVehicle from './pages/RegistrationVehicle/RegistrationVehicle';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Registration />} />
         <Route path="/registration-vehicle" element={<RegistrationVehicle />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
