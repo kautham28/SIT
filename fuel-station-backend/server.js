c40aaba4ff19d585861e0ea078038c7fc40a2c7e
Arunan19 <aruarunan19@gmail.com>
2025-05-12 05:06:11 +0530

    feat: add initial frontend setup with Vite and React, including assets and styles

--- fuel-station-backend/server.js
+++ fuel-station-backend/server.js
@@ -14,6 +14,8 @@ const usersRouter = require('../fuel-station-backend/routes/user_route.js');
 const FuelTransactionRouter = require('../fuel-station-backend/routes/station_fuel_transactions_route.js');
 const vehicleCategoryRouter = require('../fuel-station-backend/routes/vehicle_category_route.js');
 const fuelStationRouter = require('../fuel-station-backend/routes/fuel_stations_route.js');
+const MotorTrafficRouter = require('./routes/motor_traffic_route.js');
+const fuelStationOperatorsRouter = require('../fuel-station-backend/routes/fuel_station_operators.js');
 
 app.use('/api/vehicles', vehiclesRouter);
 app.use('/api/vehicle_fuel_transactions', vehicleFuelTransactionsRouter);
@@ -21,6 +23,9 @@ app.use('/api/station_fuel_transactions', FuelTransactionRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/vehicle_categories', vehicleCategoryRouter);
 app.use('/api/fuel_stations', fuelStationRouter);
+app.use('/api/motor_traffic', MotorTrafficRouter);
+app.use('/api/fuel_station_operators', fuelStationOperatorsRouter);
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to the API');
